8fe515738593ec7b56af4126140e8cc787577512
davegregg <davegregg@gmail.com>
2017-02-22 15:31:20 -0500

    Oops, most of the way through Explorer while on Master. Switching now.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org' do
   gem 'sinatra'
   gem 'test-unit', require: 'test/unit'
   gem 'rack-test', require: 'rack/test'
-  gem 'faker'
+  gem 'ffaker'
 end
--- first_app.rb
+++ first_app.rb
@@ -1,1 +1,17 @@
 require 'sinatra'
+require 'ffaker'
+
+get '/lorem/:type' do
+  case :type
+  when 'standard', 'ipsum'
+    FFaker::Lipsum.paragraphs(3)
+  end
+end
+
+get '/:name' do
+  ':name, we welcome you. Join us. Leave all aseity behind. Be one with the Oneness in which all is naught.'
+end
+
+get '/' do
+  'Welcome!'
+end
